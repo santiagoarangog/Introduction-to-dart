579b39efeaa8e474bc7523ac4db7b23fbd2d6916
Santiago Arango Gutierrez <santiago9606@gmail.com>
2022-06-23 21:03:46 -0500

    Add map

---
+++ map.dart
@@ -0,0 +1,13 @@
+void main(){
+
+  Map persona = {
+    'nombre' : 'Santiago',
+    'edad' : 26,
+    'soltero' : false, 
+    true : false,
+    1 : 100,
+    2 : 500
+  };
+
+  print(persona['nombre']);
+}
